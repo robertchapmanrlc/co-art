632e96f632dd4fbd5a6e70dbf2d383926bf1da9b
Robert Chapman <chapmanrobert12@gmail.com>
2024-07-08 15:40:20 -0500

    Implemented basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,30 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Co Art</h1>
+    <main>
+      <h1 className={styles.title}>Co Art</h1>
+      <div className={styles.mainContent}>
+        <section className={styles.players}>
+          <div className={styles.player}>
+            <div className={styles.playerPic} />
+            <p>Player 1</p>
+          </div>
+          <div className={styles.player}>
+            <div className={styles.playerPic} />
+            <p>Player 2</p>
+          </div>
+          <div className={styles.player}>
+            <div className={styles.playerPic} />
+            <p>Player 3</p>
+          </div>
+          <div className={styles.player}>
+            <div className={styles.playerPic} />
+            <p>Player 4</p>
+          </div>
+        </section>
+        <canvas width={400} height={400} className={styles.drawingCanvas} />
+        <aside className={styles.art}>Art</aside>
+      </div>
     </main>
   );
 }
